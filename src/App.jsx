097a14210e60cbf0e848ba6f46a824d3090017c2
pablonuñez10 <nunezbenitezpablo@gmail.com>
2024-03-21 10:40:07 -0300

    estado-carrito

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,106 @@
 import { Header } from "./components/Header"
 import { Guitar } from "./components/Guitar"
 import { db } from "./data/db"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 function App() {
-    const [data, setData] = useState(db)
-    const [cart, setCart] = useState([])
-
-    function addToCart (item) {
-        const itemExists = cart.findIndex( guitar => guitar.id === item.id)
-        if (itemExists >= 0) {
-            const updatedCart = [...cart]
-            updatedCart[itemExists].quantity++
-            setCart[updatedCart]
-        } else {
-            item.quantity = 1
-            setCart ([...cart, item])
-        }
- 
-
+    const initialCart = () => {
+        const localStorageCart = localStorage.getItem('cart')
+        return localStorageCart ? JSON.parse(localStorageCart) : []
     }
 
-   return (
-    <>
-        <Header 
-            cart = {cart}
-        />
-        <main className="container-xl mt-5">
-            <h2 className="text-center">Nuestra Colección</h2>
+    const [data, setData] = useState(db);
+    const [cart, setCart] = useState(initialCart);
 
-            <div className="row mt-5">
-                {data.map((guitar) => {
-                    return (
-                        <Guitar 
-                            key={guitar.id}
-                            guitar={guitar}
-                            setCart={setCart}
-                            addToCart={addToCart}
-                        />
-                    )
-                })}    
+    const MAX_ITEMS = 5
+    const MIN_ITEMS = 1
 
-            </div>
-        </main>
-        <footer className="bg-dark mt-5 py-5">
-            <div className="container-xl">
-                <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
-            </div>
-        </footer>
-    </>
-  )
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
+
+    function addToCart(item) {
+        const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+        if (itemExists >= 0 ) {
+            if(cart[itemExists].quantity >= MAX_ITEMS) return 
+            const updatedCart = [...cart]
+            updatedCart[itemExists].quantity++
+            setCart(updatedCart);
+        } else {
+            item.quantity = 1;
+            setCart([...cart, item])
+        }
+    }
+
+    function removeFromCart(id) {
+        setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+    }
+    function decreaseFromCart (id) {
+        const updatedCart = cart.map(item => {
+            if (item.id === id && item.quantity > MIN_ITEMS) {
+                return {
+                    ...item,
+                    quantity: item.quantity - 1
+                }
+            }
+            return item
+        })
+        setCart(updatedCart)
+     }
+
+    function increaseQuantity(id) {
+        const updatedCart = cart.map(item => {
+            if (item.id === id && item.quantity < MAX_ITEMS) {
+                return {
+                    ...item,
+                    quantity: item.quantity + 1
+                }
+            }
+            return item
+        })
+        setCart(updatedCart)
+    }
+
+    function clearCart () {
+        setCart([])
+    }
+
+
+
+
+    return (
+
+        <>
+            <Header
+                cart={cart}
+                removeFromCart={removeFromCart}
+                increaseQuantity={increaseQuantity}
+                decreaseFromCart={decreaseFromCart}
+                clearCart={clearCart}
+            />
+            <main className="container-xl mt-5">
+                <h2 className="text-center">Nuestra Colección</h2>
+
+                <div className="row mt-5">
+                    {data.map((guitar) => {
+                        return (
+                            <Guitar
+                                key={guitar.id}
+                                guitar={guitar}
+                                setCart={setCart}
+                                addToCart={addToCart}
+                            />
+                        )
+                    })}
+
+                </div>
+            </main>
+            <footer className="bg-dark mt-5 py-5">
+                <div className="container-xl">
+                    <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+                </div>
+            </footer>
+        </>
+    )
 }
 
-export {App}
+export { App }
